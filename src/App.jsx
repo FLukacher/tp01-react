448413731f1073af98b1c386ca55b77a23139a78
FACUNDO LUKACHER <49091985@ort>
2026-04-22 13:52:58 -0300

    localSotrage

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,10 @@ import Formulario from "./components/Formulario";
 import Listado from "./components/Listado";
 import { useState, useEffect } from "react";
 
-function App() {
-  const defaultCitas = [
-    {
-      id: 1,
-      mascota: "Nina",
-      propietario: "Martin",
-      fecha: "2021-08-05",
-      hora: "08:20",
-      sintomas: "Le duele la pierna",
-    },
-    {
-      id: 2,
-      mascota: "Sifon",
-      propietario: "Flecha",
-      fecha: "2023-08-05",
-      hora: "09:24",
-      sintomas: "Duerme mucho",
-    },
-    {
-      id: 3,
-      mascota: "Floki",
-      propietario: "Ari",
-      fecha: "2023-08-05",
-      hora: "16:15",
-      sintomas: "No está comiendo",
-    },
-    {
-      id: 4,
-      mascota: "Kiva",
-      propietario: "Luk",
-      fecha: "2023-01-05",
-      hora: "16:15",
-      sintomas: "Esta re dura",
-    },
-  ];
+const defaultCitas = [];
 
+function App() {
+ 
   const [citas, setCitas] = useState(() => {
     try {
       const stored = localStorage.getItem("citas-vet");
